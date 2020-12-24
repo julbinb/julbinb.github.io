5721d0e6a87fe322cec04c7157a63d6ebb736f0a
Julia Belyakova <julbinb@gmail.com>
2020-12-24 13:24:25 -0500

    clean js and some html after merge

--- Gemfile
+++ Gemfile
@@ -2,4 +2,3 @@ source "https://rubygems.org"
 #gemspec
 gem "jekyll-theme-hydeout", "~> 3.4"
 gem "kramdown-parser-gfm"
-
--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,17 +1,24 @@
+/* flips the visibility of an element */
 function toggle_visibility(id) {
     var e = document.getElementById(id);
     e.style.display = (e.style.display == 'none') ? 'block' : 'none';
 }
 
+/* flips the visibility of an element and
+   changes the text of the corresponding button, e.g.
+   "Abstract" -> "Abstract (hide)" when name is "Abstract"
+ */
 function toggle_visibility_name(id, name) {
+    // div element
     var e = document.getElementById(id);
     if (e === null) {
       console.log("couldn't fine element " + id);
     }
+    // button corresponsing to the element e
     var b = document.getElementById(id + "Button");
     if (e.style.display == 'none') {
-		e.style.display = 'block';
-		b.innerHTML = name + ' (hide)';
+		  e.style.display = 'block';
+		  b.innerHTML = name + ' (hide)';
     }
     else {
 	    e.style.display = 'none';
